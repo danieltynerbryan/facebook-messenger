5efcbd7f15a894db4f67142cb35ea238f8934950
Tyner-Bryan <dtynerbryan@scrippsnetworks.com>
2017-11-06 12:51:20 -0500

    bumping to 2.11

--- lib/facebook/messenger/subscriptions.rb
+++ lib/facebook/messenger/subscriptions.rb
@@ -6,7 +6,7 @@ module Facebook
     module Subscriptions
       include HTTParty
 
-      base_uri 'https://graph.facebook.com/v2.6/me'
+      base_uri 'https://graph.facebook.com/v2.11/me'
 
       format :json
 
